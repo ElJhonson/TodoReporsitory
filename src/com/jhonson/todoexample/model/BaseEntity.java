336de02eae6295f81589be73ae4503eb658d10d2
ElJhonson34 <angel.alvarez20@hotmail.com>
2024-06-06 22:06:19 -0600

    Correction in the program structture

--- src/com/jhonson/todoexample/model/BaseEntity.java
+++
@@ -1,7 +0,0 @@
-package com.jhonson.todoexample.model;
-
-public class BaseEntity {
-    protected Integer id;
-    protected Priority priority;
-    protected Status status;
-}
--- src/com/jhonson/todoexample/model/Task.java
+++ src/com/jhonson/todoexample/model/Task.java
@@ -1,4 +1,20 @@
 package com.jhonson.todoexample.model;
 
-public class Task extends BaseEntity{
+public class Task {
+    protected Integer id;
+    protected static Integer idAux;
+    protected Priority priority;
+    protected Status status;
+    protected String task;
+
+    public Task() {
+        this.id = ++idAux;
+        this.status = Status.NOCOMPLETED;
+    }
+
+    public Task(String task, Priority priority) {
+        this();
+        this.task = task;
+        this.priority = priority;
+    }
 }
